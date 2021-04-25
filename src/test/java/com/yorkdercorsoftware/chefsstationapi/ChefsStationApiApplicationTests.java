e06df99f2d01f0606d1e660cebd6f0e06d19b9e0
André <andre.victorueg@gmail.com>
2021-04-25 09:22:43 -0300

    Signed-off-by: André <andre.victorueg@gmail.com>

---
+++ src/test/java/com/yorkdercorsoftware/chefsstationapi/ChefsStationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yorkdercorsoftware.chefsstationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefsStationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
